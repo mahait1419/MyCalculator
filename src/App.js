fe0eb932592b6bd5d48e8e8ee5aacbcd75a75894
unknown <mahait1419@gmail.com>
2021-09-16 13:24:41 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Keys from './Keys';
+import ResultCom from './ResultCom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      result: ""
+    }
+  }
+  onClick = button => {
+    if (button === "=") {
+      this.calculate()
+    }
+    else if (button === "C") {
+      this.reset()
+    }
+    else if (button === "CE") {
+      this.backspace()
+    }
+    else {
+      this.setState({
+        result: this.state.result + button
+      })
+    }
+  };
+
+  calculate = () => {
+    var checkRes = ''
+    if (this.state.result.includes('--')) {
+      checkRes = this.state.result.replace('--', '+')
+    }
+    else {
+      checkRes = this.state.result
+    }
+
+    try {
+      this.setState({
+        result: (eval(checkRes) || "") + ""
+      })
+    } catch (e) {
+      this.setState({
+        result: "error"
+      })
+    }
+  };
+
+  reset = () => {
+    this.setState({
+      result: ""
+    })
+  };
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    })
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="calc-body">
+          <h1>MY CALCULATOR</h1>
+          <h1> </h1>
+          <br />
+          <ResultCom result={this.state.result} />
+          <Keys onClick={this.onClick} />
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
